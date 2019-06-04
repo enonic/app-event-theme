f09b7b49549375e6730a4d6c9ecce3222d855c00
Sandra Moen <sandramoen01@gmail.com>
2019-06-04 08:43:17 +0200

    #4 Added banner part

--- src/main/resources/site/parts/banner/banner.js
+++ src/main/resources/site/parts/banner/banner.js
@@ -8,15 +8,29 @@ exports.get = function(req) {
 
 	/* ### Collect ### */
 	var content = libPortal.getContent(); // Get current content that is viewed. See the docs for JSON format.
-	var component = libPortal.getComponent(); // Or, get config (if any) for this particular part. See the docs for JSON format.
+	var component = libPortal.getComponent(); // Or, get config (if any) for this particular part. See the docs for JSON format.	
+    var config = component.config;
 
 	/* ### Manipulate ### */
-	
+    var haveCountDown = config.haveCountDown;
+    var title = config.title;
+    var date = config.date;
+    var location = config.location;
+    var ticketUrl = config.ticketUrl;
+    var backgroundImage = config.backgroundImage;	
+
+    /* log.info('banner.js JSON %s', JSON.stringify(config, null, 4)); */
+
 	/* ### Prepare ### */
 	var model = {
 		content: content,
 		component: component,
-		someText: component.config["someText"]
+		haveCountDown: haveCountDown,
+		title: title,
+		date: date,
+		location: location,
+		ticketUrl: ticketUrl,
+		backgroundImage: backgroundImage
 	};
 
 	/* ### Return ### */
