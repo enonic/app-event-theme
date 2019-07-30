09e64900c1ee08bb4bed764ec4f8f81d706e1ca1
Sandra Moen <sandramoen01@gmail.com>
2019-07-30 10:47:20 +0200

    miscellaneous

--- src/main/resources/assets/js/pages/default.js
+++ src/main/resources/assets/js/pages/default.js
@@ -25,3 +25,23 @@ $(function () {
         console.log('default.js: Error adding listener to subscribe-button');
     }
 });
+
+window.addEventListener('DOMContentLoaded', (event) => { // activate the script after everything has loaded
+    // When the user scrolls the page, execute myFunction 
+    window.onscroll = function() {myFunction()};
+
+    // Get the header
+    var header = document.getElementById("myHeader");
+
+    // Get the offset position of the navbar
+    var sticky = header.offsetTop;
+
+    // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
+    function myFunction() {
+        if (window.pageYOffset > sticky) {
+            header.classList.add("sticky");
+        } else {
+            header.classList.remove("sticky");
+        }
+    }
+});
