c86b9220a996bd814cfac93fb6d57079d6b97390
Sandra Moen <sandramoen01@gmail.com>
2019-08-07 09:39:22 +0200

    Added scroll to top button to footer

--- src/main/resources/assets/js/pages/default.js
+++ src/main/resources/assets/js/pages/default.js
@@ -27,8 +27,11 @@ $(function () {
 });
 
 window.addEventListener('DOMContentLoaded', (event) => { // activate the script after everything has loaded
+    /* HEADER SCROLLING 
+        https://www.w3schools.com/howto/howto_js_sticky_header.asp) 
+    */
     // When the user scrolls the page, execute myFunction 
-    window.onscroll = function() {myFunction()};
+    window.onscroll = function() {scrollFunction()};
 
     // Get the header
     var header = document.getElementById("myHeader");
@@ -37,11 +40,33 @@ window.addEventListener('DOMContentLoaded', (event) => { // activate the script 
     var sticky = header.offsetTop;
 
     // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-    function myFunction() {
+    function scrollFunction() {
         if (window.pageYOffset > sticky) {
             header.classList.add("sticky");
         } else {
             header.classList.remove("sticky");
         }
     }
+
+    /* TO-TOP BUTTON 
+        https://www.w3schools.com/howto/howto_js_scroll_to_top.asp
+        https://stackoverflow.com/questions/15935318/smooth-scroll-to-top/48837437
+    */
+    function sendUserToTop(event) {
+        // document.body.scrollTop = document.documentElement.scrollTop = 0; // For Safari, Chrome, Firefox, IE and Opera
+        const scrollToTop = () => {
+            const c = document.documentElement.scrollTop || document.body.scrollTop;
+            if (c > 0) {
+                window.requestAnimationFrame(scrollToTop);
+                let scrollSpeed = 16; // for slower motion of the scrolling, increase this number.
+                window.scrollTo(0, c - c / scrollSpeed);
+            }
+        };
+        scrollToTop();
+    }
+
+    try {
+        var toTopButton = document.getElementById('toTop');
+        toTopButton.addEventListener('click', sendUserToTop);
+    } catch(err) {}
 });
