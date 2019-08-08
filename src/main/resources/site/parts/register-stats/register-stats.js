d4fd445e5a89b3575a5a4987920e2f9dbd9bd280
Sandra Moen <sandramoen01@gmail.com>
2019-08-08 11:39:28 +0200

    bugfixes

--- src/main/resources/site/parts/register-stats/register-stats.js
+++ src/main/resources/site/parts/register-stats/register-stats.js
@@ -4,25 +4,34 @@ var libThymeleaf = require('/lib/thymeleaf');
 
 var viewFile = resolve('register-stats.html'); // TODO: This is not the view-file you're looking for ... or is it?
 
-exports.get = function(req) {
+exports.get = function (req) {
 
 	/* ### Collect ### */
-	var content = libPortal.getContent(); // Get current content that is viewed. See the docs for JSON format.
-	var component = libPortal.getComponent(); // Or, get config (if any) for this particular part. See the docs for JSON format.	
-    var config = component.config;
-	
-	/* ### Manipulate ### */	
-    /* log.info('register-stats.js JSON %s', JSON.stringify(config.backgroundImage, null, 4)); */
+	let content = libPortal.getContent(); // Get current content that is viewed. See the docs for JSON format.
+	let component = libPortal.getComponent(); // Or, get config (if any) for this particular part. See the docs for JSON format.	
+	let config = component.config;
+
+	let site = libPortal.getSite();
+	let properName = app.name.replace(/\./g, '-');
+	let siteConfig = site.x[properName].siteConfig;
+
+	/* ### Manipulate ### */
+	let oneDay = 24 * 60 * 60 * 1000; // hours*minutes*seconds*milliseconds
+	let firstDate = new Date(siteConfig.fromDate);
+	let secondDate = new Date(siteConfig.toDate);
+	let diffDays = Math.round(Math.abs((firstDate.getTime() - secondDate.getTime()) / (oneDay))) + 1 | 0;
+
+	/* log.info('register-stats.js JSON %s', JSON.stringify(diffDays, null, 4)); */
 
 	/* ### Prepare ### */
-	var model = {
+	let model = {
 		content: content,
-        component: component,
-        speakers: config.speakers,
-        seats: config.seats,
-        tickets: config.tickets,
-        days: config.days,
-        backgroundImage: config.backgroundImage,
+		component: component,
+		speakers: config.speakers,
+		seats: config.seats,
+		tickets: config.tickets,
+		days: diffDays,
+		backgroundImage: config.backgroundImage,
 	};
 
 	/* ### Return ### */
