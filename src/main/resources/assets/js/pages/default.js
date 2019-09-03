d66c9f0b80ac6403f7723dbe53544d8c014eda9b
Sandra Moen <sandramoen01@gmail.com>
2019-09-03 16:00:36 +0200

    Responsive burgermenu fix

--- src/main/resources/assets/js/pages/default.js
+++ src/main/resources/assets/js/pages/default.js
@@ -14,6 +14,29 @@ $(function () {
         });
     }
 
+    function toggleNavbar() {
+        element = document.getElementById('navbarNav');
+        if (element.classList.contains('show')) {
+            element.classList.remove("show");
+        } else {
+            element.classList.add("show");
+        }
+    }
+
+    function toggleNavLinkHasChildren(event) {
+        let element = this;
+        let elementMenu = document.getElementById('submenu-' + element.getAttribute('subChoice'));
+
+        if (elementMenu.dataset.toggle === 'false') {
+            elementMenu.style.display = 'inherit';
+            elementMenu.dataset.toggle = 'true';
+        } else if (elementMenu.dataset.toggle === 'true') {
+            elementMenu.style.display = 'none';
+            elementMenu.dataset.toggle = 'false';
+        }
+        event.preventDefault();
+    }
+
     // listeners
     try {
         document.getElementById('subscribe-button').addEventListener('click', function (e) {
@@ -21,7 +44,23 @@ $(function () {
             sendRegisterData(url);
         });
     }
-    catch (err) {}
+    catch (err) { }
+
+    try {
+        document.getElementsByClassName('navbar-toggler')[0].addEventListener('click', function (e) {
+            toggleNavbar();
+        });
+    }
+    catch (err) { }
+
+    try {
+        console.log('try');
+        for (let i = 0; i < document.getElementsByClassName('nav-link-hasChildren').length; i++) {
+            let element = document.getElementsByClassName('nav-link-hasChildren')[i];
+            element.addEventListener('click', toggleNavLinkHasChildren);
+        }
+    }
+    catch (err) { console.log('catch'); }
 });
 
 window.addEventListener('DOMContentLoaded', (event) => { // activate the script after everything has loaded
@@ -29,7 +68,7 @@ window.addEventListener('DOMContentLoaded', (event) => { // activate the script 
         https://www.w3schools.com/howto/howto_js_sticky_header.asp) 
     */
     // When the user scrolls the page, execute myFunction 
-    window.onscroll = function() {scrollFunction()};
+    window.onscroll = function () { scrollFunction() };
 
     // Get the header
     var header = document.getElementById("myHeader");
@@ -66,5 +105,5 @@ window.addEventListener('DOMContentLoaded', (event) => { // activate the script 
     try {
         var toTopButton = document.getElementById('toTop');
         toTopButton.addEventListener('click', sendUserToTop);
-    } catch(err) {}
+    } catch (err) { }
 });
